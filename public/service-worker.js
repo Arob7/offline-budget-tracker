30ce9817a0ccec048aea36f7474fa59965478d50
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-10-18 21:38:24 -0400

    app works offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,3 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -8,9 +5,11 @@ const FILES_TO_CACHE = [
   "/style.css",
   "/app.js",
   "/icons/icon-192x192.png",
-  "/icons",
   "/icons/icon-512x512.png",
 ];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
